23bf605ceae01d6d7f1ad697a4623762a21ff939
zahra liaghatmand <zahra.liaghatmand@gmail.com>
2024-05-23 09:37:28 +0330

    Add modifyCollections

---
+++ ModifyCollections/ModifyCollections/ModifyCollections/Program.cs
@@ -0,0 +1,8 @@
+﻿//Because LINQ works with IEnumerable and IEnumerable doesn't have
+//modification methods, LINQ methods won't effect the original collection
+
+var numbers  = new List<int> { 1, 2, 3, 4, 5 };
+var nuumbersWithSix = numbers.Append(6);
+
+Console.WriteLine("numbers: " + string.Join(", ",numbers));
+Console.WriteLine("numbersWitSix: " + string.Join(", ", nuumbersWithSix));
